a499ab8b9503e2ad52de3d3c9d1fcffe08f16499
Nathan Rapport <n.rapport@gmail.com>
2016-01-13 12:51:26 -0800

    refactored

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,23 @@
 class SearchesController < ApplicationController
+  #GET
+  #/searches/new
   def new
     @search = Search.new
     @categories = Book.uniq.pluck(:category)
   end
 
+  #POST
+  #/searches (but it runs when you are leaving /searches/new)
   def create
     @search = Search.create(search_params)
     redirect_to @search
   end
 
+  #GET
+  #/searches/1
   def show
     @search = Search.find(params[:id])
+    @books = @search.search_books
   end
 
   private
